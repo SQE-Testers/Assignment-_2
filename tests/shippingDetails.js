eb3aa59340182bda67786d9557bd7a42e39f4a36
mohsinali1051 <84640319+mohsinali1051@users.noreply.github.com>
2022-11-05 17:58:06 +0500

    Opened url Magento then added a Bag into cart then reached at shipping details

---
+++ tests/shippingDetails.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "Testing magento" : browser => {
+
+        browser.url("https://magento.jsmartfix.com/")
+        //browser.pause(1000)
+
+        browser.click("#ui-id-6 > span:nth-child(2)")
+        //browser.pause(1000)
+
+        browser.click("#narrow-by-list2 > dd > ol > li:nth-child(1) > a")
+        //browser.pause(1000)
+
+        browser.click("#maincontent > div.columns > div.column.main > div.products.wrapper.grid.products-grid > ol > li:nth-child(1) > div > a > span > span > img")
+        browser.pause(3000)
+
+        browser.click("#product-addtocart-button > span")
+        browser.pause(3000)
+
+        browser.click("body > div.page-wrapper > header > div.header.content > div.minicart-wrapper > a")
+        browser.pause(1000)
+
+        browser.click("#top-cart-btn-checkout")
+        browser.pause(1000)
+
+        //browser.setValue('#customer-email',[string,"mohsin@gamil.com"])
+        browser.sendKeys('#customer-email', 'nightwatchooo');
+        browser.pause(3000)
+
+        
+        //browser.click("body > div.page-wrapper > header > div.header.content > div.minicart-wrapper > a")
+        //browser.pause(3000)
+    }
+}
